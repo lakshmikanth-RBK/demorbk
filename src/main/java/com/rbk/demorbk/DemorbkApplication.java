e05fd21dfe63d71ad23d0ecdddb3be272fb483cd
Lakshmikanth <lazzurbk@gmail.com>
2023-12-31 18:31:57 +0530

    First_commit

---
+++ src/main/java/com/rbk/demorbk/DemorbkApplication.java
@@ -0,0 +1,13 @@
+package com.rbk.demorbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorbkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemorbkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbk/demorbk/DemorbkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbk.demorbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemorbkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
